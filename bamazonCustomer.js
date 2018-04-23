ff8a23415e2816a9e7bbf7df7efdc7219c667519
SallyTemple <pfende@gmail.com>
2018-04-23 15:55:07 -0700

    update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,6 +7,29 @@ var databaseConnection = mysql.createConnection({
 host: "local host",
 port:3306,
 user:"root",
-password:" ",
+password:"",
 database: "bamazon",
 });
+
+connection.connect(function (err) {
+	if (err) throw err;
+	console.log("Error connecting to database");
+      displayProducts();
+});
+
+//Ask the user what item he or she wants to purchase
+
+function displayProducts() {
+      inquirer.prompt([{
+            name:"id",
+            type:"input",
+            message:"What is the id of the product you want to purchase?"
+      },
+      {name:"quantity",
+      type:"input",
+      message:"How many unit of the product you want to buy?"
+
+      }]).then function(data){
+            connection.connect
+      })
+}
